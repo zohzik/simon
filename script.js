6f9a7548f028e786b12df22702e4c996758cf904
zohzik3 <taymyr1026@gmail.com>
2015-03-12 18:41:27 +0000

    dosnt seem to be working

--- script.js
+++ script.js
@@ -1,45 +1,97 @@
 $(document).ready(function(){
-    var lost = false;
-    var score=0;
-    var numOfBlips;
-         numOfBlips = score + 1;
-         for(var i = numOfBlips; i>0;i--){
-             var selection = Math.floor(Math.random() * 4) + 1;
-             switch(selection){
-                 case 1:
-                        $("#topLeft").animate({
-                            color:"#FF3333"
-                        }, 500);
-                        $("#topLeft").animate({
-                            color:"red"
-                        }, 500);
+    $('#start').click(function(){
+        var lost = false;
+        var score=0;
+        var blips = {};
+        while(!lost){
+            var numOfBlips = score + 1;
+             for(var i = numOfBlips; i>0;i--){
+                 var selection = Math.floor(Math.random() * 4) + 1;
+                 switch(selection){
+                     case 1:
+                            $("#topLeft").animate({
+                                color:"#FF3333"
+                            }, 500);
+                            $("#topLeft").animate({
+                                color:"red"
+                            }, 500);
+                            blips.push('topleft');
+                         break;
+                     case 2:
+                         $("#topLeft").animate({
+                                color:"##3333FF"
+                            }, 500);
+                            $("#topLeft").animate({
+                                color:"#blue"
+                            }, 500);
+                            blips.push('topright');
+                         break;
+                     case 3:
+                         $("#topLeft").animate({
+                                color:"#FFFF66"
+                            }, 500);
+                            $("#topLeft").animate({
+                                color:"#yellow"
+                            }, 500);
+                            blips.push('bottomleft');
+                         break;
+                     case 4:
+                         $("#topLeft").animate({
+                                color:"#66C266"
+                            }, 500);
+                            $("#topLeft").animate({
+                                color:"green"
+                            }, 500);
+                            blips.push('bottomright');
+                         break;
+                     default:
                      break;
-                 case 2:
-                     $("#topLeft").animate({
-                            color:"##3333FF"
-                        }, 500);
-                        $("#topLeft").animate({
-                            color:"#blue"
-                        }, 500);
-                     break;
-                 case 3:
-                     $("#topLeft").animate({
-                            color:"#FFFF66"
-                        }, 500);
-                        $("#topLeft").animate({
-                            color:"#yellow"
-                        }, 500);
-                     break;
-                 case 4:
-                     $("#topLeft").animate({
-                            color:"#66C266"
-                        }, 500);
-                        $("#topLeft").animate({
-                            color:"green"
-                        }, 500);
-                     break;
-                 default:
-                 break;
+                 }
              }
-         }
+             
+             var finished = false; 
+             var counter = 1;
+             while(!finished){
+                 $(".button").click(function(){
+                     if($(this)===$('#topLeft') && blips[counter]==="topleft"){
+                         counter++;
+                         if(counter === blips.length){
+                             finished=true;
+                             score++;
+                         }
+                     }else if($(this)===$('#topRight') && blips[counter]==='topright'){
+                         counter++;
+                         if(counter === blips.length){
+                             finished=true;
+                             score++;
+                         }
+                     }else if($(this)===$('#bottomLeft') && blips[counter]==='bottomleft'){
+                         counter++;
+                         if(counter === blips.length){
+                             finished=true;
+                             score++;
+                         }
+                     }else if($(this)===$('#bottomRight') && blips[counter]==='bottomright'){
+                         counter++;
+                         if(counter === blips.length){
+                             finished=true;
+                             score++;
+                         }
+                     }else{
+                         finished=true;
+                         lost=true;
+                     }
+                 });
+             }
+             
+             $('#score').html(score.toString());
+        }
+    });
 });
+
+
+
+
+
+
+
